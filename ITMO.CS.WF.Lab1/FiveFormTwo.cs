a261cfb3344cc3e8fcc76829cabb3d014077f816
glebv <glebv@inbox.ru>
2022-07-02 21:27:28 +0300

    Add project files.

---
+++ ITMO.CS.WF.Lab1/FiveFormTwo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ITMO.CS.WF.Lab1
+{
+    public partial class FiveFormTwo : Form
+    {
+        public FiveFormTwo()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FiveFormOne f = new FiveFormOne();
+            f.Show();
+        }
+    }
+}
